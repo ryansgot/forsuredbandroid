1e0d049f5cbe3699e06ef02e36a1ef677bf8e5c0
ryansgot <ryansgot@gmail.com>
2015-09-14 20:17:44 -0500

    create a ContentProvider within the forsuredbandroidlib; change testapp mainfest to reflect this; add some testing

---
+++ forsuredbandroid/src/main/java/com/forsuredb/provider/FSDefaultProvider.java
@@ -0,0 +1,93 @@
+package com.forsuredb.provider;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+import com.forsuredb.ForSure;
+
+/**
+ * <p>
+ *     The default content provider that integrates with ForSure
+ * </p>
+ * @author Ryan Scott
+ */
+public class FSDefaultProvider extends ContentProvider {
+
+    public FSDefaultProvider() {}
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        if (!ForSure.canResolve(uri)) {
+            return null;
+        }
+        return ForSure.resolve(uri).table().getMimeType();
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        if (!ForSure.canResolve(uri)) {
+            return null;
+        }
+
+        final String tableName = ForSure.resolve(uri).table().getName();
+        long rowId = ForSure.getWritableDatabase().insertWithOnConflict(tableName, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+        if (rowId != -1) {
+            final Uri insertedItemUri = ContentUris.withAppendedId(uri, rowId);
+            getContext().getContentResolver().notifyChange(insertedItemUri, null);
+            return insertedItemUri;
+        }
+        return null;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        if (!ForSure.canResolve(uri)) {
+            return 0;
+        }
+
+        final String tableName = ForSure.resolve(uri).table().getName();
+        final QueryCorrector qc = new QueryCorrector(uri, selection, selectionArgs);
+        final int rowsAffected = ForSure.getWritableDatabase().update(tableName, values, qc.getSelection(), qc.getSelectionArgs());
+        if (rowsAffected != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsAffected;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        if (!ForSure.canResolve(uri)) {
+            return 0;
+        }
+
+        final String tableName = ForSure.resolve(uri).table().getName();
+        final QueryCorrector qc = new QueryCorrector(uri, selection, selectionArgs);
+        final int rowsAffected = ForSure.getWritableDatabase().delete(tableName, qc.getSelection(), qc.getSelectionArgs());
+        if (rowsAffected != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsAffected;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        if (!ForSure.canResolve(uri)) {
+            return null;
+        }
+
+        final String tableName = ForSure.resolve(uri).table().getName();
+        final QueryCorrector qc = new QueryCorrector(uri, selection, selectionArgs);
+        final Cursor cursor = ForSure.getReadableDatabase().query(tableName, projection, qc.getSelection(), qc.getSelectionArgs(), null, null, sortOrder);
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);  // <-- allows CursorLoader to auto reload
+        return cursor;
+    }
+}
---
+++ forsuredbandroid/src/main/java/com/forsuredb/provider/QueryCorrector.java
@@ -0,0 +1,110 @@
+/*
+   forsuredb, an object relational mapping tool
+
+   Copyright 2015 Ryan Scott
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+package com.forsuredb.provider;
+
+import android.net.Uri;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * <p>
+ *     Helps ensure properly formatted queries based upon the {@link Uri uri}, selection String,
+ *     and selectionArgs passed into the constructor.
+ * </p>
+ * @author Ryan Scott
+ */
+/*package*/ class QueryCorrector {
+
+    private static final String ID_SELECTION = "_id = ?";
+    private static final Pattern ID_SELECTION_PATTERN = Pattern.compile("_id *(=|IS) *\\?");
+
+    private final String selection;
+    private final String[] selectionArgs;
+
+    public QueryCorrector(Uri uri, String selection, String[] selectionArgs) {
+        final boolean isSingleRecordQuery = isSingleRecord(uri);
+        this.selection = isSingleRecordQuery ? ensureIdInSelection(selection) : selection;
+        this.selectionArgs = isSingleRecordQuery ? ensureIdInSelectionArgs(uri, selectionArgs) : selectionArgs;
+    }
+
+    public String getSelection() {
+        return selection;
+    }
+
+    public String[] getSelectionArgs() {
+        return selectionArgs;
+    }
+
+    private boolean isSingleRecord(Uri uri) {
+        try {
+            Long.parseLong(uri.getLastPathSegment());
+            return true;
+        } catch (Exception e) {
+            // do nothing
+        }
+        return false;
+    }
+
+    private String ensureIdInSelection(String selection) {
+        if (Strings.isNullOrEmpty(selection)) {
+            return ID_SELECTION;
+        }
+
+        if (!ID_SELECTION_PATTERN.matcher(selection).find()) {
+            return ID_SELECTION + " AND (" + selection + ")";    // <-- insert the _id selection if it doesn't exist
+        }
+        return selection;
+    }
+
+    private String[] ensureIdInSelectionArgs(Uri uri, String[] selectionArgs) {
+        if (!selectionWasModified(selection, selectionArgs)) {
+            return selectionArgs;
+        }
+
+        final List<String> selectionArgList = selectionArgs == null ? new ArrayList<String>() : Lists.newArrayList(selectionArgs);
+        selectionArgList.add(0, uri.getLastPathSegment());  // <-- prepend because the modified selection string specifies the _id selection first
+        return selectionArgList.toArray(new String[selectionArgList.size()]);
+    }
+
+    // If the selection was modified, then the number of ? in the selection will be one more than the length of selectionArgs
+    private boolean selectionWasModified(String selection, String[] selectionArgs) {
+        final int qMarkOccurrences = occurrencesOf('?', selection);
+        return (selectionArgs == null && qMarkOccurrences != 0) || qMarkOccurrences == selectionArgs.length + 1;
+    }
+
+    /**
+     * <p>
+     *     an approximation of the one available in the Apache Commons library's StringUtils
+     *     countMatches method geared towards counts of characters
+     * </p>
+     * @param c character to count
+     * @param str String in which to count characters
+     * @return number of times c appears in str
+     */
+    private int occurrencesOf(char c, String str) {
+        if (str == null) {
+            return 0;
+        }
+        return str.replaceAll("[^" + c + "]", "").length();
+    }
+}
