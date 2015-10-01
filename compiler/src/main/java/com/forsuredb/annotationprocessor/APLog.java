bd4e296ddb66c5a15c2ed0ed195793ee56058214
ryansgot <ryansgot@gmail.com>
2015-09-30 23:52:45 -0500

    Give APLog silent logging behavior when its methods are called prior to initialization

--- compiler/src/main/java/com/forsuredb/annotationprocessor/APLog.java
+++ compiler/src/main/java/com/forsuredb/annotationprocessor/APLog.java
@@ -17,12 +17,13 @@
  */
 package com.forsuredb.annotationprocessor;
 
+import com.forsuredb.api.FSLogger;
+
 import javax.annotation.processing.ProcessingEnvironment;
 
 public class APLog {
 
     private static final String LOG_TAG = APLog.class.getSimpleName();
-
     private static ProcessingEnvLogger log;
 
     public static void init(ProcessingEnvironment processingEnv) {
@@ -33,19 +34,19 @@ public class APLog {
     }
 
     public static void e(String tag, String message) {
-        log.e(combine(tag, message));
+        (log == null ? FSLogger.SILENT_LOG : log).e(combine(tag, message));
     }
 
     public static void i(String tag, String message) {
-        log.i(combine(tag, message));
+        (log == null ? FSLogger.SILENT_LOG : log).i(combine(tag, message));
     }
 
     public static void o(String tag, String message) {
-        log.o(combine(tag, message));
+        (log == null ? FSLogger.SILENT_LOG : log).o(combine(tag, message));
     }
 
     public static void w(String tag, String message) {
-        log.w(combine(tag, message));
+        (log == null ? FSLogger.SILENT_LOG : log).w(combine(tag, message));
     }
 
     private static String combine(String tag, String message) {
