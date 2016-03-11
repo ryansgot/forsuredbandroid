1e33c8b23a367819b834b63a2d817c647620ff8d
ryansgot <ryansgot@gmail.com>
2016-03-11 17:57:17 +0900

    demonstrate in test app with a boolean column that is not the deleted column

--- app/src/main/java/com/forsuredb/testapp/model/ProfileInfoTable.java
+++ app/src/main/java/com/forsuredb/testapp/model/ProfileInfoTable.java
@@ -13,4 +13,5 @@ public interface ProfileInfoTable extends FSGetApi {
     @FSColumn("user_id") @ForeignKey(apiClass = UserTable.class, columnName = "_id") long userId(Retriever retriever);
     @FSColumn("email_address") String emailAddress(Retriever retriever);
     @FSColumn("binary_data") byte[] binaryData(Retriever retriever);
+    @FSColumn("awesome") boolean awesome(Retriever retriever);
 }
