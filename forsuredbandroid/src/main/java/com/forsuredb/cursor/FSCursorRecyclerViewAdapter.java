4f6ab71ab168e7ce09e42a0e47ae03a1c9d9f657
ryansgot <ryansgot@gmail.com>
2016-03-30 17:27:38 +0900

    javadoc for FSCursorRecyclerViewAdapter

--- forsuredbandroid/src/main/java/com/forsuredb/cursor/FSCursorRecyclerViewAdapter.java
+++ forsuredbandroid/src/main/java/com/forsuredb/cursor/FSCursorRecyclerViewAdapter.java
@@ -4,6 +4,26 @@ import android.support.v7.widget.RecyclerView;
 
 import com.forsuredb.api.FSGetApi;
 
+/**
+ * <p>
+ *     This works best in conjunction with an {@link FSCursorLoader}, keeping a reference to your
+ *     adapter and updating the adapter with the new {@link FSCursor} in the
+ *     {@link android.app.LoaderManager.LoaderCallbacks#onLoadFinished(android.content.Loader, Object)}
+ *     implementation.
+ * </p>
+ * <p>
+ *     You should extend this class to make a cursor recycler adapter for your {@link RecyclerView}.
+ *     It's a relatively minimal implementation, but the details of stable ids and how to get the id
+ *     for any entry is taken care of for you.
+ * </p>
+ * <p>
+ *     You will not need to implement {@link #onBindViewHolder(FSCursorViewHolder, int)}, as it is
+ *     implemented here, but you do need to implement
+ *     {@link #onCreateViewHolder(android.view.ViewGroup, int)}
+ * </p>
+ * @param <G> An extension of the {@link FSGetApi} class
+ * @param <VH> An extension of the {@link FSCursorViewHolder} class
+ */
 public abstract class FSCursorRecyclerViewAdapter<G extends FSGetApi, VH extends FSCursorViewHolder> extends RecyclerView.Adapter<VH> {
 
     private FSCursor c;
@@ -45,7 +65,7 @@ public abstract class FSCursorRecyclerViewAdapter<G extends FSGetApi, VH extends
 
     /**
      * <p>
-     *     Swap cursors and close the old c
+     *     Swap cursors and close the old cursor.
      * </p>
      * @param newC the new {@link FSCursor} data
      */
